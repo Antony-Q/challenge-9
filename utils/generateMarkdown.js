8f78fe9f1fa6a77248304143dd7ba9864df3b638
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-03-30 11:31:40 -0500

    made change to license structure in generatemarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,3 @@
-function renderLicenseBadge(license) {
-  let licenseType = license.license;
-  let yourLicense = ''
-  if (licenseType === 'MIT') {
-    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
-  } else if (licenseType === 'GPLv3') {
-    yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
-  } else if (licenseType === 'GPL') {
-    yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
-  } else {
-    license.license = "N/A"
-  }
-  return yourLicense;
-};
-
 const generateMarkdown = data => {
 
   return `# ${data.title}
@@ -51,6 +36,20 @@ const generateMarkdown = data => {
     ## Questions
     If you have any questions about this project, please contact me directly at ${data.email}. You can view more of my projects at https://github.com/${data.github}.
   `;
+  function renderLicenseBadge(license) {
+    let licenseType = license.license;
+    let yourLicense = ''
+    if (licenseType === 'MIT') {
+      yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    } else if (licenseType === 'GPLv3') {
+      yourLicense = `![GPLv3 license](https://img.shields.io/badge/License-GPLv3-blue.svg)`
+    } else if (licenseType === 'GPL') {
+      yourLicense = `![GPL license](https://img.shields.io/badge/License-GPL-blue.svg)`
+    } else {
+      license.license = "N/A"
+    }
+    return yourLicense;
+  };
 }
 
 
